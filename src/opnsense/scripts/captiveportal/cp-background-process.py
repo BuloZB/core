BuloZB/core

commit 369630dbd35c0acacc7384aa997f553fdc6ca91a

Captive portal: IPv6 support (#9745)
This commit adds IPv6 support for Captive Portal by introducing a new "roaming" option, which is enabled by default. Roaming allows the synchronization of additional IPv4/IPv6 client address aliases, aggregating their accounting through ipfw and managing their state in the pf table. For IPv6, hostwatch is required to be enabled to prevent performance issues during client roaming IP synchronization. Furthermore, IPv6 can only work properly if a hostname is provided in the zone and proper AAAA records have been synthesized for the local DNS server - for a default setup, this requires the DNS64 option in Unbound to be set.

Co-authored-by: Alex Goodkind <alex@goodkind.io>

src/opnsense/scripts/captiveportal/cp-background-process.py
@@ -56,6 +56,7 @@ class CPBackgroundProcess(object):
         self.arp = ARP()
         self.cnf = Config()
         self.db = DB()
+        self.db.create()
         self._conf_zone_info = self.cnf.get_zones()
 
     def list_zone_ids(self):
@@ -118,14 +119,16 @@ class CPBackgroundProcess(object):
         if zoneid in self._conf_zone_info:
             # fetch data for this zone
             cpzone_info = self._conf_zone_info[zoneid]
-            registered_addresses = list(PF.list_table(zoneid))
+            registered_addresses_pf = set(PF.list_table(zoneid))
+            registered_addresses_ipfw = set(registered_addr_accounting.keys())
             expected_clients = self.db.list_clients(zoneid)
             concurrent_users = self.db.find_concurrent_user_sessions(zoneid)
+            allow_roaming = bool(int(cpzone_info['roaming']))
 
             # handle connected clients, timeouts, address changes, etc.
             for db_client in expected_clients:
                 # fetch ip address (or network) from database
-                cpnet = db_client['ipAddress'].strip()
+                session_ips = self.db.list_session_ips(zoneid, db_client['sessionId'])
 
                 # there are different reasons why a session should be removed, check for all reasons and
                 # use the same method for the actual removal
@@ -158,8 +161,8 @@ class CPBackgroundProcess(object):
                             drop_session_reason = "remove concurrent session %s" % db_client['sessionId']
                             delete_reason = "User-Request"
 
-                    # if mac address changes, drop session. it's not the same client
-                    current_arp = self.arp.get_by_ipaddress(cpnet)
+                    # if mac address changes, drop session. it's not the same client. Use the "primary IP" to determine this
+                    current_arp = self.arp.get_by_ipaddress(db_client['ipAddress'])
                     if current_arp is not None and current_arp['mac'] != db_client['macAddress']:
                         drop_session_reason = "mac address changed for session %s" % db_client['sessionId']
                         delete_reason = "Admin-Reset"
@@ -170,39 +173,47 @@ class CPBackgroundProcess(object):
                             and time.time() - float(db_client['startTime']) > db_client['acc_session_timeout']:
                             drop_session_reason = "accounting limit reached for session %s" % db_client['sessionId']
                             delete_reason = "Session-Timeout"
-                elif db_client['authenticated_via'] == '---mac---':
-                    # detect mac changes
-                    current_ip = self.arp.get_address_by_mac(db_client['macAddress'])
-                    if current_ip is not None and db_client['ipAddress'] != current_ip:
+
+                if drop_session_reason is not None:
+                    # remove session
+                    syslog.syslog(syslog.LOG_NOTICE, drop_session_reason)
+                    for ip in session_ips:
+                        self._remove_client(zoneid, ip)
+                    self.db.del_client(zoneid, db_client['sessionId'], delete_reason)
+                    continue
+
+                # if primary IP changed, update db accordingly. Not relevant for static IP-authenticated clients
+                if db_client['authenticated_via'] != '---ip---':
+                    current_ips = self.arp.get_all_addresses_by_mac(db_client['macAddress'])
+                    if len(current_ips) > 0 and db_client['ipAddress'] != current_ips[0]:
                         if db_client['ipAddress'] != '':
                             # remove old ip
                             self._remove_client(zoneid, db_client['ipAddress'])
-                        self.db.update_client_ip(zoneid, db_client['sessionId'], current_ip)
-                        self._add_client(zoneid, current_ip)
+                        self.db.update_client_ip(zoneid, db_client['sessionId'], current_ips[0])
+                        self._add_client(zoneid, current_ips[0])
+                        db_client['ipAddress'] = current_ips[0]
 
-                # check session, if it should be active, validate its properties
-                if drop_session_reason is None:
-                    # registered client, but not active in pf or missing accounting according to ipfw (after reboot)
-                    if cpnet and (
-                        cpnet not in registered_addresses or
-                        cpnet not in registered_addr_accounting
-                    ):
-                        self._add_client(zoneid, cpnet)
+                # session should be active, validate its properties
+                if allow_roaming:
+                    # this will add the "primary" IP as well, but both list_session_ips and update_roaming_ips will return a deduplicated set
+                    session_ips = self.db.update_roaming_ips(zoneid, db_client['sessionId'], self.arp.get_all_addresses_by_mac(db_client['macAddress']))
                 else:
-                    # remove session
-                    syslog.syslog(syslog.LOG_NOTICE, drop_session_reason)
-                    self._remove_client(zoneid, cpnet)
-                    self.db.del_client(zoneid, db_client['sessionId'], delete_reason)
+                    # may have been updated if primary IP changed
+                    session_ips = {db_client['ipAddress']}
 
-            # if there are addresses/networks in the underlying pf table which are not in our administration,
-            # remove them from pf.
-            for registered_address in registered_addresses:
-                address_active = False
-                for db_client in expected_clients:
-                    if registered_address == db_client['ipAddress']:
-                        address_active = True
-                        break
-                if not address_active:
+                to_add = (session_ips - registered_addresses_pf) | (session_ips - registered_addresses_ipfw)
+                if session_ips and to_add:
+                    for ip in to_add:
+                        self._add_client(zoneid, ip)
+
+            # remove any address from pf/ipfw that isn't expected
+            expected_addresses = set()
+            # need to query again as clients may have been updated
+            for db_client in self.db.list_clients(zoneid):
+                expected_addresses.update(self.db.list_session_ips(zoneid, db_client['sessionId']))
+
+            for registered_address in (registered_addresses_pf | registered_addresses_ipfw):
+                if registered_address not in expected_addresses:
                     self._remove_client(zoneid, registered_address)
 
 def main():

src/etc/inc/plugins.inc.d/captiveportal.inc
@@ -91,7 +91,6 @@ function captiveportal_firewall($fw)
             }
 
             foreach ($zone->interfaces->getValues() as $intf) {
-                // allow DNS
                 $fw->registerFilterRule(
                     1,
                     [
@@ -111,7 +110,7 @@ function captiveportal_firewall($fw)
                 foreach (['80', '443'] as $to_port) {
                     $rdr_port = $to_port === '443' ? (8000 + (int)$zoneid) : (9000 + (int)$zoneid);
 
-                    // forward to localhost if not authenticated
+                    // forward to localhost if not authenticated (IPv4)
                     $fw->registerForwardRule(
                         2,
                         [
@@ -133,7 +132,29 @@ function captiveportal_firewall($fw)
                         ]
                     );
 
-                    // Allow access to the captive portal
+                    // forward to localhost if not authenticated (IPv6)
+                    $fw->registerForwardRule(
+                        2,
+                        [
+                            'interface' => $intf,
+                            'pass' => true,
+                            'nordr' => false,
+                            'ipprotocol' => 'inet6',
+                            'protocol' => 'tcp',
+                            'from' => "<__captiveportal_zone_{$zoneid}>",
+                            'from_not' => true,
+                            'to' => "<__captiveportal_zone_{$zoneid}>",
+                            'to_not' => true,
+                            'to_port' => $to_port,
+                            'target' => $intf . 'ip',
+                            'localport' => $rdr_port,
+                            'natreflection' => 'disable',
+                            'log' => true,
+                            'descr' => "Redirect to Captive Portal (zone {$zoneid})",
+                            '#ref' => "ui/captiveportal#edit={$uuid}"
+                        ]
+                    );
+
                     $proto = $to_port === '443' ? 'https' : 'http';
                     $fw->registerFilterRule(
                         2,
@@ -152,7 +173,6 @@ function captiveportal_firewall($fw)
                     );
                 }
 
-                // block all non-authenticated users
                 $fw->registerFilterRule(
                     3,
                     [

src/opnsense/mvc/app/controllers/OPNsense/CaptivePortal/Api/AccessController.php
@@ -50,12 +50,13 @@ class AccessController extends ApiControllerBase
     protected function clientSession(string $zoneid)
     {
         $backend = new Backend();
-        $allClientsRaw = $backend->configdpRun("captiveportal list_clients", [$zoneid]);
-        $allClients = json_decode($allClientsRaw, true);
+        $allClients = json_decode($backend->configdpRun("captiveportal list_clients", [$zoneid]), true);
+        $clientIp = $this->getClientIp();
+
         if ($allClients != null) {
             // search for client by ip address
             foreach ($allClients as $connectedClient) {
-                if ($connectedClient['ipAddress'] == $this->getClientIp()) {
+                if (in_array($clientIp, $connectedClient['ipAddresses'])) {
                     // client is authorized in this zone according to our administration
                     $connectedClient['clientState'] = 'AUTHORIZED';
                     return $connectedClient;
@@ -64,7 +65,7 @@ class AccessController extends ApiControllerBase
         }
 
         // return Unauthorized including authentication requirements
-        $result = ['clientState' => "NOT_AUTHORIZED", "ipAddress" => $this->getClientIp()];
+        $result = ['clientState' => "NOT_AUTHORIZED", "ipAddress" => $clientIp];
         $mdlCP = new CaptivePortal();
         $cpZone = $mdlCP->getByZoneID($zoneid);
         if ($cpZone != null && (string)$cpZone->extendedPreAuthData == '1') {
@@ -103,14 +104,15 @@ class AccessController extends ApiControllerBase
     protected function getClientMac($ip)
     {
         if (empty($this->arp)) {
-            /* currently this only matches ipv4 properly, for ipv6 we need to unpack both rows and offered parameter */
             $data = json_decode((new Backend())->configdRun('hostwatch dump'), true) ?? [];
             if (!empty($data['rows'])) {
                 foreach ($data['rows'] as $row) {
-                    $this->arp[$row[2]] = $row[1];
+                    // remove scope from IPv6 address if present (e.g., fe80::1%em0 -> fe80::1)
+                    $this->arp[$row[2]] = explode('%', $row[1])[0];
                 }
             }
         }
+
         return $this->arp[$ip] ?? null;
     }
 
@@ -306,10 +308,11 @@ class AccessController extends ApiControllerBase
                             if (array_key_exists('session_timeout', $authProps) || $cpZone->alwaysSendAccountingReqs == '1') {
                                 $backend->configdpRun(
                                     "captiveportal set session_restrictions",
-                                    array((string)$cpZone->zoneid,
+                                    [
+                                        (string)$cpZone->zoneid,
                                         $CPsession['sessionId'],
                                         $authProps['session_timeout'] ?? null,
-                                        )
+                                    ]
                                 );
                             }
                         }

src/opnsense/mvc/app/controllers/OPNsense/CaptivePortal/forms/dialogZone.xml
@@ -37,6 +37,16 @@
         </grid_view>
     </field>
     <field>
+        <id>zone.roaming</id>
+        <type>checkbox</type>
+        <label>Client roaming</label>
+        <help>Allow a connecting client to use multiple IPs (bound to the same MAC) over the course of its session. This option is needed for maximum IPv6 compatibility and also affects IPv4 clients.</help>
+        <grid_view>
+            <type>boolean</type>
+            <formatter>boolean</formatter>
+        </grid_view>
+    </field>
+    <field>
         <id>zone.authservers</id>
         <label>Authenticate using</label>
         <type>select_multiple</type>

src/opnsense/mvc/app/library/OPNsense/System/Status/CaptivePortalStatus.php
@@ -1,0 +1,56 @@
+<?php
+
+/*
+ * Copyright (C) 2026 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\System\Status;
+
+use OPNsense\System\AbstractStatus;
+use OPNsense\System\SystemStatusCode;
+use OPNsense\Hostdiscovery\Hostwatch;
+
+class CaptivePortalStatus extends AbstractStatus
+{
+    public function __construct()
+    {
+        $this->internalPriority = 2;
+        $this->internalPersistent = true;
+        $this->internalTitle = gettext('Captive Portal IPv6 support');
+        $this->internalIsBanner = true;
+        $this->internalScope[] = '/ui/captiveportal*';
+    }
+
+    public function collectStatus()
+    {
+        if ((new Hostwatch())->general->enabled->isEmpty()) {
+            $this->internalMessage = gettext(
+                'The host discovery service is disabled, which is required for Captive Portal IPv6 compatibility. ' .
+                'You can enable it under Interfaces -> Neighbors -> Automatic Discovery.'
+            );
+            $this->internalStatus = SystemStatusCode::WARNING;
+        }
+    }
+}

src/opnsense/mvc/app/models/OPNsense/CaptivePortal/CaptivePortal.xml
@@ -1,6 +1,6 @@
 <model>
     <mount>//OPNsense/captiveportal</mount>
-    <version>1.0.4</version>
+    <version>1.0.5</version>
     <description>Captive portal application model</description>
     <items>
         <zones>
@@ -33,6 +33,10 @@
                     <Multiple>Y</Multiple>
                     <Service>CaptivePortal</Service>
                 </authservers>
+                <roaming type="BooleanField">
+                    <Default>1</Default>
+                    <Required>Y</Required>
+                </roaming>
                 <alwaysSendAccountingReqs type="BooleanField">
                     <Default>0</Default>
                     <Required>Y</Required>

src/opnsense/mvc/app/views/OPNsense/CaptivePortal/clients.volt
@@ -72,6 +72,29 @@
                 requestHandler: function(request) {
                     request['selected_zones'] = $("#zone-selection").val();
                     return request;
+                },
+                formatters: {
+                    ipAddress: function(column, row) {
+                        const ips = row.ipAddresses || [];
+
+                        if (!ips.length) {
+                            return $('<span>', { class: 'text-muted', text: '-' })[0].outerHTML;
+                        }
+
+                        return $('<span>', {
+                            'data-toggle': 'tooltip',
+                            'data-placement': 'top',
+                            title: ips.join('\n')
+                        }).append(ips.map(ip => $('<div>').text(ip).html()).join('<br>'))[0].outerHTML;
+                    },
+                    userName: function(column, row) {
+                        // Extract IP from username@ip format and show just username
+                        let userName = row.userName || '';
+                        if (userName && userName.indexOf('@') >= 0) {
+                            return userName.split('@')[0] || userName;
+                        }
+                        return userName;
+                    }
                 }
             }
         });
@@ -79,6 +102,17 @@
         $("#zone-selection-wrapper").detach().insertBefore('#grid-clients-header .search');
     });
 </script>
+
+<style>
+    [data-column-id="ipAddress"] {
+        white-space: normal !important;
+        word-break: break-all;
+        line-height: 1.5;
+        max-height: 80px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+</style>
 
 <ul class="nav nav-tabs" data-tabs="tabs" id="maintabs"></ul>
 <div class="tab-content content-box col-xs-12 __mb">
@@ -91,14 +125,14 @@
             <tr>
                 <th data-column-id="sessionId" data-type="string" data-identifier="true" data-visible="false">{{ lang._('Session') }}</th>
                 <th data-column-id="zoneid" data-width="7em"  data-type="string" data-visible="false">{{ lang._('Zoneid') }}</th>
-                <th data-column-id="userName" data-type="string">{{ lang._('Username') }}</th>
-                <th data-column-id="macAddress" data-type="string"  data-width="12em" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('MAC address') }}</th>
-                <th data-column-id="ipAddress" data-type="string"  data-width="12em" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('IP address') }}</th>
-                <th data-column-id="bytes_in" data-type="string"  data-width="8em" data-formatter="bytes" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('Bytes (in)') }}</th>
-                <th data-column-id="bytes_out" data-type="string" data-width="8em" data-formatter="bytes"  data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('Bytes (out)') }}</th>
-                <th data-column-id="startTime" data-type="datetime">{{ lang._('Connected since') }}</th>
-                <th data-column-id="last_accessed" data-type="datetime" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('Last accessed') }}</th>
-                <th data-column-id="commands" data-searchable="false" data-width="7em" data-formatter="commands" data-sortable="false">{{ lang._('Commands') }}</th>
+                <th data-column-id="userName" data-type="string" data-width="10em" data-formatter="userName">{{ lang._('Username') }}</th>
+                <th data-column-id="macAddress" data-type="string" data-width="10em" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('MAC address') }}</th>
+                <th data-column-id="ipAddress" data-type="string" data-width="9em" data-formatter="ipAddress" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('IP Address') }}</th>
+                <th data-column-id="bytes_in" data-type="string" data-width="7em" data-formatter="bytes" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('Bytes (in)') }}</th>
+                <th data-column-id="bytes_out" data-type="string" data-width="7em" data-formatter="bytes" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('Bytes (out)') }}</th>
+                <th data-column-id="startTime" data-type="datetime" data-width="10em">{{ lang._('Connected since') }}</th>
+                <th data-column-id="last_accessed" data-type="datetime" data-width="10em" data-css-class="hidden-xs hidden-sm" data-header-css-class="hidden-xs hidden-sm">{{ lang._('Last accessed') }}</th>
+                <th data-column-id="commands" data-searchable="false" data-width="5em" data-formatter="commands" data-sortable="false">{{ lang._('Commands') }}</th>
             </tr>
         </thead>
         <tbody>

commit 2b43ee5b87948347ac5a704c484001d169f059e4

captive portal: regression in 369630d, allowed addresses missing from session ips
If a client is allowed through a statically configured IP, the MAC
becomes secondary, but in the roaming case the set of actual IPs
relied on the MAC address. Since a statically configured IP may not
have a MAC active at a given time, but we do want to keep this IP in
the captive portal pf zone at all time, we merge the primary IP
with the roaming IPs. As a set this will deduplicate automatically.

While here, no MAC address lookup was performed in the case of a
static IP, add it here so it can be actively seen in the sessions
GUI.

Closes https://github.com/opnsense/core/issues/10124

src/opnsense/scripts/captiveportal/cp-background-process.py
@@ -192,11 +192,16 @@ class CPBackgroundProcess(object):
                         self.db.update_client_ip(zoneid, db_client['sessionId'], current_ips[0])
                         self._add_client(zoneid, current_ips[0])
                         db_client['ipAddress'] = current_ips[0]
+                else:
+                    # if authenticated via 'allowed addresses', check if we should update the mac address (for display purposes)
+                    current_arp = self.arp.get_by_ipaddress(db_client['ipAddress'])
+                    if current_arp is not None and current_arp['mac'] != db_client['macAddress']:
+                        self.db.update_client_mac(zoneid, db_client['sessionId'], current_arp['mac'])
 
                 # session should be active, validate its properties
                 if allow_roaming:
-                    # this will add the "primary" IP as well, but both list_session_ips and update_roaming_ips will return a deduplicated set
-                    session_ips = self.db.update_roaming_ips(zoneid, db_client['sessionId'], self.arp.get_all_addresses_by_mac(db_client['macAddress']))
+                    # merge primary IP here, if authenticated via ip, the roaming ips may not contain the statically configured IP if there is no MAC known.
+                    session_ips = {db_client['ipAddress']} | self.db.update_roaming_ips(zoneid, db_client['sessionId'], self.arp.get_all_addresses_by_mac(db_client['macAddress']))
                 else:
                     # may have been updated if primary IP changed
                     session_ips = {db_client['ipAddress']}

src/opnsense/scripts/captiveportal/lib/db.py
@@ -202,6 +202,18 @@ class DB(object):
                     """, {'zoneid': zoneid, 'sessionid': sessionid, 'ip_address': ip_address})
         self._connection.commit()
 
+    def update_client_mac(self, zoneid, sessionid, mac_address):
+        """ change client mac address
+        """
+        cur = self._connection.cursor()
+        cur.execute("""update cp_clients
+                       set    mac_address = :mac_address
+                       where  deleted = 0
+                       and    zoneid = :zoneid
+                       and    sessionid = :sessionid
+                    """, {'zoneid': zoneid, 'sessionid': sessionid, 'mac_address': mac_address})
+        self._connection.commit()
+
     def update_roaming_ips(self, zoneid, sessionid, ip_addresses=[]):
         """Update roaming IP addresses for a session to exactly match the provided list.
         Returns the current set of IP addresses stored in the database.